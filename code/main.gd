6cd4c9b777fe738002e4ee48a4673d0888cfb4ef
Max <gurskimaksym@gmail.com>
2024-09-04 19:21:16 +0200

    Enemy spawn + buttons fix
    This time I finished enemy spawn logic and fixed button texture error

--- code/main.gd
+++ code/main.gd
@@ -13,51 +13,23 @@ func _ready():
 	Global.speed_plus = 95
 	$Camera2D/AnimationPlayer.play("shake") #NAPRAWIĆ!
 
-
 func _physics_process(delta):
 	$CanvasL/Panel/punkty.text = "SCORE:" + str(Global.score)
 
 func _on_timer_timeout():         #SPAWNING ENEMIES
-	var position1 = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
-	var position2 = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
-	var position3 = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
-	var position4 = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
-	#ADD NO SPAWNING ZONE
-	
-	spawn_enemies(position1)
-	await get_tree().create_timer(1.5).timeout
-	spawn_enemies2(position2)
-	await get_tree().create_timer(3).timeout
-	spawn_enemies3(position3)
-	await get_tree().create_timer(2.5).timeout
-	spawn_enemies4(position4)
+	for i in range(4):
+		var position = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
+		
+		while position.distance_to($CharacterBody2D.position) < 100:
+			position = Vector2(rngX.randf_range(77, 1843), rngY.randf_range(88, 850))
+		spawn_enemy(position)
+		await get_tree().create_timer(1.5).timeout
 
-func spawn_enemies(position1: Vector2):
+func spawn_enemy(position: Vector2):
 	var enemy = enemy_scene.instantiate()
-	enemy.global_position = position1
+	enemy.global_position = position
 	add_child(enemy)
-	print("Enemy spawned at:", position1)
-	Global.speed_plus += Global.normal_addition
-	
-func spawn_enemies2(position2: Vector2):
-	var enemy2 = enemy_scene.instantiate()
-	enemy2.global_position = position2
-	add_child(enemy2)
-	print("Enemy spawned at:", position2)
-	Global.speed_plus += Global.normal_addition
-	
-func spawn_enemies3(position3: Vector2):
-	var enemy3 = enemy_scene.instantiate()
-	enemy3.global_position = position3
-	add_child(enemy3)
-	print("Enemy spawned at:", position3)
-	Global.speed_plus += Global.normal_addition
-	
-func spawn_enemies4(position4: Vector2):
-	var enemy4 = enemy_scene.instantiate()
-	enemy4.global_position = position4
-	add_child(enemy4)
-	print("Enemy spawned at:", position4)
+	print("Enemy spawned at:", position)
 	Global.speed_plus += Global.normal_addition
 
 func _stop() -> void:

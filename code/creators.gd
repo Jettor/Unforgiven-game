0a20d39cd7d2fb170530d23bb6f645cc8dfae42a
Max <gurskimaksym@gmail.com>
2025-03-17 19:57:14 +0100

    Ending cutscene
    Added a cutscene, that plays after you complete the game. It's only DEMO tho.
    Song used: Plastic tree - てふてふ

--- code/creators.gd
+++ code/creators.gd
@@ -3,8 +3,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$song.play()
-	$Button.grab_focus()
-
+	$Opener.play("opening")
+	$Title/AnimationPlayer.play("up")
+	$BoxContainer/AnimatedSprite2D.play("new_animation")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("kill_game"):
@@ -19,3 +20,11 @@ func _on_button_mouse_entered():
 	$hover.play()
 func _on_button_focus_entered():
 	$hover.play()
+
+
+func _on_cutscene_end_timeout():
+	$Title/AnimationPlayer.play("hide_panel")
+	$Button/Button_animation.play("show_btn")
+	$Title/AnimationPlayer.play("hide_pic")
+func _on_button_animation_animation_finished(anim_name):
+	$Button.grab_focus()
